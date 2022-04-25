7b6085ed5f5636639dc5320187920e52e97014b2
Git Cho <joseohyun0201@gmail.com>
2022-04-25 10:35:43 +0900

    database 만들고 bean생성

---
+++ SpringJDBC/src/main/java/kr/hs/study/config/BeanConfigClass.java
@@ -0,0 +1,29 @@
+package kr.hs.study.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@ComponentScan(basePackages={"kr.hs.study.beans","kr.hs.study.dao"})
+public class BeanConfigClass {
+	@Bean
+	public BasicDataSource source() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName("oracle.jdbc.Oracledriver");
+		source.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		source.setUsername("scott");
+		source.setPassword("tiger");
+		
+		return source;
+	}
+	
+	@Bean 
+	//db에 접속해서 실제 쿼릴르 실행한느 애를 bean으로 등록
+	public JdbcTemplate db(BasicDataSource source) {
+		JdbcTemplate db = new JdbcTemplate(source);
+		return db;
+	}
+}
